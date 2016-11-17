fef098ca7a1bd5c21a9049efdc02a5dc6f1b323a
noby <n.fujioka@abacusmaths.info>
2016-11-17 16:41:00 +0000

    defines relationship with eloquent

--- resources/views/cards/index.blade.php
+++ resources/views/cards/index.blade.php
@@ -5,7 +5,7 @@
   <ul>
     @foreach ($cards as $card)
       <li>
-        {{ $card -> title }}
+        <a href="/cards/{{ $card->id }}">{{ $card -> title }}</a>
       </li>
     @endforeach
 </ul>
