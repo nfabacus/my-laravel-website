b18d400904c9c206a89e093f6efa52a67c0510d8
noby <n.fujioka@abacusmaths.info>
2016-11-20 07:48:59 +0000

    adds form

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,10 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>myLaravelSite</title>
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
     <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
   </head>
   <body>
-    @yield('content')
+
+    <div class="container">
+      @yield('content')
+    </div>
+
 
   </body>
 </html>
