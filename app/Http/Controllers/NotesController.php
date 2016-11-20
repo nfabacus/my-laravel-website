b060f6744d725698fdc3c6fc111a1401ad7115eb
noby <n.fujioka@abacusmaths.info>
2016-11-20 08:52:24 +0000

    adds addNote function to card class

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -10,10 +10,27 @@ class NotesController extends Controller
 {
     public function store(Request $request, Card $card)
     {
-      $note = new Note;
-      $note->body = $request->body;
+      // option1
+      // $note = new Note;
+      // $note->body = $request->body;
+      // $card->notes()->save($note);
 
-      $card->notes()->save($note);
+      // option2
+      // $note = new Note(['body' => $request->body]);
+      // $card->notes()->save($note);
+
+      //option 3
+      // $card->notes()->create([
+      //   'body' => $request->body
+      // ]);
+
+      // option 4
+      // $card->notes()->create($request->all());
+
+      // option 5
+      $card->addNote(
+        new Note($request->all())
+      );
 
       return back();
     }
