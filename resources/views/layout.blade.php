098a3ecf6ed8cc528d5e75bd3d4ee4b8882a1c70
noby <n.fujioka@abacusmaths.info>
2016-11-21 23:00:36 +0000

    WIP - updating Recrds

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>myLaravelSite</title>
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
     <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
   </head>
   <body>
